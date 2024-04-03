8d211ad776426af3fa88a61db004fe9bdde4dbf4
Sam Sargent <samsarge@hotmail.co.uk>
2024-04-03 21:05:40 +0100

    Support both JPG and PNG output types Spice up the CLI with clap

--- README.md
+++ README.md
@@ -1,7 +1,30 @@
 # split-gif
 
-Save frames of a GIF as PNG
+Save frames of a GIF
 
 ## Usage
 
-`cargo run -- path_to_file.gif`
+```
+Split a GIF file into frames and save them as images. Allows different file type ouputs
+
+Usage: split-gif --input-path <INPUT_PATH> --output-type <OUTPUT_TYPE>
+
+Options:
+  -i, --input-path <INPUT_PATH>
+          Input GIF path
+
+  -o, --output-type <OUTPUT_TYPE>
+          Output file type of the frames
+
+          [possible values: png, jpg]
+
+  -h, --help
+          Print help (see a summary with '-h')
+
+  -V, --version
+          Print version
+```
+
+## Supported output formats
+- JPEG
+- PNG
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::env;
+
 use std::path::Path;
 
 use image::codecs::gif::GifDecoder;
@@ -8,24 +8,45 @@ use image::{Frame, AnimationDecoder, ImageFormat, DynamicImage};
 use std::fs::File;
 use std::io::BufReader;
 
+
+use clap::Parser;
+
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum OutputType {
+    Png,
+    Jpg
+}
+
+
+#[derive(Parser, Debug)]
+#[command(version = "1", about = "Split GIF into frames", long_about = "Split a GIF file into frames and save them as images. Allows different file type ouputs")]
+struct Args {
+    /// Input GIF path
+    #[arg(short, long)]
+    input_path: String,
+
+    /// Output file type of the frames.
+    #[arg(short, long)]
+    output_type: OutputType,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = Args::parse();
 
 
-    let gif_path = args.get(1).unwrap();
-    let gif_path = Path::new(gif_path);
+    let gif_path = args.input_path;
+    let output_type = args.output_type;
+
+    let gif_path = Path::new(&gif_path);
 
     // Decode a gif into frames
     let frames = read_into_frames(gif_path);
 
-
     let mut frame_count = 0;
 
     frames.iter().for_each(|frame|{
-        let path = format!("frame_{}.png", frame_count);
-        let path = Path::new(&path);
+        save_frame(frame.to_owned(), frame_count, output_type.to_owned());
         frame_count += 1;
-        save_frame(frame.to_owned(), path);
     })
 }
 
@@ -41,16 +62,32 @@ fn read_into_frames(path: &Path) -> Vec<Frame> {
     });
 
     let frames = decoder.into_frames();
-    let frames = frames.collect_frames().expect("error decoding gif");
+    
 
-    frames
+    frames.collect_frames().expect("error decoding gif")
 }
 
-fn save_frame(frame: Frame, path: &Path) {
+fn save_frame(frame: Frame, frame_count: i32, output_type: OutputType) {
+
     let img_buffer = frame.into_buffer();
     let img_buffer = DynamicImage::ImageRgba8(img_buffer);
 
-    img_buffer.save_with_format(path, ImageFormat::Png).unwrap_or_else(|err|{
-        println!("{}", err)
-    });
+    match output_type {
+        OutputType::Jpg => {
+            let path = format!("frame_{}.jpg", frame_count);
+            let path = Path::new(&path);
+            let img_buffer = img_buffer.into_rgb8();
+            img_buffer.save_with_format(path, ImageFormat::Png).unwrap_or_else(|err|{
+                eprintln!("{}", err)
+            });
+        },
+        OutputType::Png => {
+            let path = format!("frame_{}.png", frame_count);
+            let path = Path::new(&path);
+            img_buffer.save_with_format(path, ImageFormat::Png).unwrap_or_else(|err|{
+                eprintln!("{}", err)
+            });
+        }
+    }
+
 }
